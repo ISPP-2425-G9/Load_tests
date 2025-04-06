baf341faf026d0c1cd205209e3082da8b8910d61
dangalcan <megamagolas@gmail.com>
2025-04-06 15:43:32 +0200

    fix: fix get_obituary_receivers test

--- customer/CustomerReceivers.py
+++ customer/CustomerReceivers.py
@@ -6,4 +6,4 @@ class CustomerReceivers(BaseCustomer):
     @task
     def get_obituary_receivers(self):
         obituary_id = self.obituary_id
-        self.client.get(f"/getReceivers/obituary/{obituary_id}", headers=self.headers)
+        self.client.get(f"/api/receiver/getReceivers/obituary/{obituary_id}", headers=self.headers)
